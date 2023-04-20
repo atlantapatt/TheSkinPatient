0c0f4b34a36bcfa9adcb5acb86f14c9e18351ad0
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-04-19 17:51:43 -0700

    deploy attmpt2

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :routines
   resources :users
 
-  root 'birds#index'
+  root 'users#index'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
