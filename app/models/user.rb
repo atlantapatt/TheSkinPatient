5bcc6f2aff1e747ad91741bffb84ce38dec09c73
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-04-19 17:29:49 -0700

    try to update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+    has_many :routines
+    has_many :reviews
+    has_one :wishlist
+    has_many :my_products
+
+    has_many :products, through: :my_products
 
     has_secure_password
-    validates :username, prescence: true, uniqueness: true
-    validates :password, prescence: true 
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true 
 end
