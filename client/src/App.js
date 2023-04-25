3fcc1b050d08aebf6ae3e2fad1d603b93fd2a893
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-04-25 14:40:05 -0700

    navbar continue

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { UserContext } from './Context/UserContext'
 import { Switch, Route } from 'react-router-dom';
 import Account from './Account';
 import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   const {user, setUser} = useContext(UserContext)
   return (
     <div>
+      <Navbar/>
       <Switch>
         <Route exact path='/'>
           <Home/>
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,7 +1,30 @@
+import { useHistory } from "react-router-dom";
+
 function Navbar() {
+
+    const history = useHistory()
+
+    function myAccountRoute() {
+        // let account = '/account'
+        // history.push(account)
+        console.log('account')
+    }
     return ( 
-        <div>
-            NAVBAR
+        <div className="nav">
+            <div className="nav-items">
+                <p>Welcome!</p>
+                <div className="img">
+                    LOGO
+                </div>
+                <div className="dropdown-trigger">
+                    ICON
+                </div>
+                <div className="dropdown-menu">
+                    <p onClick={myAccountRoute}>Account</p>
+                    <p>Products</p>
+                    <p>Logout</p>
+                </div>
+            </div>
         </div>
      );
 }
