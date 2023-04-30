ced7fca88efe35378a846643c9f6fbd6b5fac424
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-04-30 16:15:17 -0700

    wishlist map image showing as undefined still3

--- client/src/Components/AccountComponents/ProductsAccount.js
+++ client/src/Components/AccountComponents/ProductsAccount.js
@@ -2,16 +2,16 @@ function ProductsAccount({item}) {
     console.log(item)
     return ( 
         <div className="item-card">
-            <div className="image">
-                {/* <img src={item.image}></img> */}
+            {/* <div className="image">
+                <img src={item.image}></img>
             </div>
             <br></br>
             <div className="item-text">
                 <h4>{item.name}</h4>
                 <h5>{item.brand}</h5>
                 <p>{item.category}</p>
-            </div>
-            
+            </div> */}
+            HELLO
         </div>
      );
 }
