f7f008f2294a44634b77af33ae14fda7f7ab5a69
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-04-30 17:18:59 -0700

    fixed wishlist mapping2

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -1,5 +1,11 @@
 class MyProductsController < ApplicationController
 
+    def index
+        user = User.find_by(id: session[:user_id])
+        myProducts = MyProduct.find_by(id: user)
+        products = Product.where(id: myProducts)
+        render json: products, include: :product
+    end
 
     def create
         my_products = MyProducts.create(my_products_params)
