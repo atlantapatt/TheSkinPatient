844b4a67e563223d56f948e1c4a8e6c87524a94c
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-05-02 15:15:18 -0700

    pop up test

--- client/src/Components/ConfrimPopUp.js
+++ client/src/Components/ConfrimPopUp.js
@@ -2,7 +2,7 @@ function ConfirmPopUp({setConfirmed}) {
     return ( 
         <div className="confirmed">
             <h4>Added!</h4>
-            <button onClick={setConfirmed(false)}>OK</button>
+            <button onClick={(() => setConfirmed(false))}>OK</button>
         </div>
      );
 }
