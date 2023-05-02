62e1f1fd26d0e380ae70a5176092bc81b4820610
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-05-01 23:20:13 -0700

    top three

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -30,7 +30,7 @@ function Home({homeReviews, topThree, user}) {
  let myTopThree = topThree.map((item) => {
     console.log(item)
     return  <div className="wishlist-div">
-        {/* <TopThree item={item.products} /> */}
+        <TopThree item={item} />
     </div>
     
  })
