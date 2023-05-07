4532cb13fe72f3805e9d33ac25b033667b4cbc92
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-05-07 16:51:07 -0700

    reviews check username3

--- client/src/Components/ReviewCard.js
+++ client/src/Components/ReviewCard.js
@@ -7,14 +7,14 @@ function ReviewCard({review}) {
         if (review.user !== null) {
             setUsername(review.user.username)
         }
-    })
+    },[])
     console.log(username)
 
 //review.user.username causing errors
     return (
         <div className="review-div">
             <div className="review-text">
-                {/* <p>{review.user.username}</p> */}
+                <p>{username}</p>
                 <p>{review.info}</p>
                 <p>{review.rating}/5</p>
             </div>
