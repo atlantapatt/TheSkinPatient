d37399bf72d2fd81d50ecdac55a4efbae2277cca
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-05-07 16:03:39 -0700

    css and add usernames

--- client/src/Components/ReviewCard.js
+++ client/src/Components/ReviewCard.js
@@ -4,7 +4,7 @@ function ReviewCard({review}) {
     return (
         <div className="review-div">
             <div className="review-text">
-                {/* <p>{review.user.username}</p> */}
+                <p>{review.user.username}</p>
                 <p>{review.info}</p>
                 <p>{review.rating}/5</p>
             </div>
